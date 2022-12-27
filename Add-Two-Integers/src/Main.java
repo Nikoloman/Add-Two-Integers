96394a67fe1b28ec9be3679877697ea933881955
Nikoloman <hectorvel52@gmail.com>
2022-12-27 12:41:01 -0600

    First ever problem I've solved from Leetcode, it was really easy tho

---
+++ Add-Two-Integers/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 5;
+
+        int result = num1 + num2;
+        System.out.println("the result of the sum is: " + result);
+    }
+}
