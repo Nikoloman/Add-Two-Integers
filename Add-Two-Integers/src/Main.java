46906132e2f73cd841dfb64a4fbad04539045594
Nikoloman <hectorvel52@gmail.com>
2022-12-27 12:55:30 -0600

    First ever problem I've solved from Leetcode, it was really easy tho

--- Add-Two-Integers/src/Main.java
+++ Add-Two-Integers/src/Main.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Main { //URL of the problem: https://leetcode.com/problems/add-two-integers/
     public static void main(String[] args) {
         int num1 = 12;
         int num2 = 5;
